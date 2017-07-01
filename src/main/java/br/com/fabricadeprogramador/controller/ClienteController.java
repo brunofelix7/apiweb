31f11329955e5288a187478f8e8f5ced59a07302
Bruno <bruno@synctech.com.br>
2017-07-01 11:25:37 -0300

    arquivo web.xml

--- src/main/java/br/com/fabricadeprogramador/controller/ClienteController.java
+++ src/main/java/br/com/fabricadeprogramador/controller/ClienteController.java
@@ -1,7 +1,6 @@
 package br.com.fabricadeprogramador.controller;
 
 import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
